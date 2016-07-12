cef908f3c315c41b51a04ba4f7b18093466620c6
foooomio <foooomio@gmail.com>
2016-07-12 23:38:40 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+ruby '2.3.1'
+
+gem 'sinatra'
+gem 'rest-client'
+gem 'line-bot-api'
---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'line/bot'
+
+def bot
+  @bot ||= Line::Bot::Client.new do |config|
+    config.channel_id = ENV['LINE_CHANNEL_ID']
+    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+    config.channel_mid = ENV['LINE_CHANNEL_MID']
+  end
+end
+
+post '/callback' do
+  signature = request.env['HTTP_X_LINE_CHANNELSIGNATURE']
+  unless bot.validate_signature(request.body.read, signature)
+    error 400 do 'BAD Request' end
+  end
+
+  receive_request = Line::Bot::Receive::Request.new(request.env)
+
+  receive_request.data.each do |message|
+    case message.content
+    when Line::Bot::Message::Text
+      client.send_text(
+        to_mid: message.from_mid,
+        text: message.content[:text] + 'じゃない',
+      )
+    end
+  end
+
+  'OK'
+end
