53d3bba2514696c89dcb169bee13a854ca310a6e
foooomio <foooomio@gmail.com>
2016-07-13 20:37:35 +0900

    fix

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ post '/callback' do
     when Line::Bot::Message::Location
       bot.send_text(
         to_mid: message.from_mid,
-        text: message.latitude + ', ' + message.longitude,
+        text: message[:latitude] + ', ' + message[:longitude],
       )
     end
   end
